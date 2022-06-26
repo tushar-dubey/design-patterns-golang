e2df7a89d2c268e92ee1e10309e49824c443fdb2
tushar-dubey <tdiitg@gmail.com>
2022-06-26 17:38:37 +0530

    added example of bridge pattern

--- bridge/main/main.go
+++ bridge/main/main.go
@@ -5,10 +5,15 @@ import "fmt"
 // Renderer We want to render circle triangle and square with Vector and Raster rendering
 type Renderer interface {
 	RenderCircle(radius float32)
+	RenderEquilateralTriangle(side int)
 }
 
 type VectorRenderer struct {
 	//
+}
+
+func (v *VectorRenderer) RenderEquilateralTriangle(side int) {
+	fmt.Println("Drawing an Equilateral triangle with side: ", side)
 }
 
 func (v *VectorRenderer) RenderCircle(radius float32) {
@@ -17,6 +22,10 @@ func (v *VectorRenderer) RenderCircle(radius float32) {
 
 type RasterRenderer struct {
 	//
+}
+
+func (r *RasterRenderer) RenderEquilateralTriangle(side int) {
+	fmt.Println("Rendering an Equilateral triangle with side: ", side)
 }
 
 func (r *RasterRenderer) RenderCircle(radius float32) {
@@ -36,6 +45,26 @@ func (c *Circle) Draw() {
 	c.renderer.RenderCircle(c.radius)
 }
 
-func main() {
+type EquilateralTriangle struct {
+	side     int
+	renderer Renderer
+}
 
+func NewEquilateralTriangle(side int, renderer Renderer) *EquilateralTriangle {
+	return &EquilateralTriangle{side: side, renderer: renderer}
+}
+
+func (e *EquilateralTriangle) Draw() {
+	e.renderer.RenderEquilateralTriangle(e.side)
+}
+
+func main() {
+	rasterRenderer := RasterRenderer{}
+	vectorRenderer := VectorRenderer{}
+
+	circle := NewCircle(&rasterRenderer, 10)
+	circle.Draw()
+
+	triangle := NewEquilateralTriangle(15, &vectorRenderer)
+	triangle.Draw()
 }
